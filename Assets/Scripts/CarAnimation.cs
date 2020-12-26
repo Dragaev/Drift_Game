6608f3657c640ba21265b4cd688c897afd3a2401
Max Dragaev <dragaev@MacBook-Air-Max.local>
2020-12-26 14:35:10 +0700

    add car selection and scene management

---
+++ Assets/Scripts/CarAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAnimation : MonoBehaviour
+{
+    [SerializeField] private Vector3 finalPosition;
+    private Vector3 initialPosition;
+    void Awake()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, finalPosition, 0.1f);
+    }
+    private void OnDisable()
+    {
+        transform.position = initialPosition;
+    }
+}
